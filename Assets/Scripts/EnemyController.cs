25656e79b2634da280d45a75e0bc0218c5315122
Glonfindel <glonfindel1995@gmail.com>
2018-10-08 13:23:53 +0200

    ai part 1

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+
+    private Transform target;
+    private NavMeshAgent ai;
+
+	void Start ()
+	{
+	    target = GameObject.FindGameObjectWithTag("Player").transform;
+	    ai = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+	{
+	    float distance = Vector3.Distance(target.position, transform.position);
+
+	    if (distance <= lookRadius)
+	    {
+	        ai.SetDestination(target.position);
+
+	        if (distance <= ai.stoppingDistance)
+	        {
+	            FaceTarget();
+
+	        }
+	    }
+	}
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
