18c55d317e0522e33c417dd6dabbb78749501bb7
Marta Skrajda <m.skrajda.1996@gmail.com>
2018-10-28 09:44:26 +0100

    Scriptable Objects

---
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="CharacterWithoutWeapon",menuName ="Data/Character/WithoutWeapon")]
+public class CharacterData : ScriptableObject {
+
+    [Header("Combat stats")]
+    public int hp;
+    public int damage;
+    public float attackSpeed;
+    [Header("Movement stats")]
+    public float movementSpeed;
+    public float rotateSpeed;
+    public float jumpPower;
+
+}
