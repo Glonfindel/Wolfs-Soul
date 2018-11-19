ed3ad1975b7ac0dc8803f34a91359d0b3d7780b5
Glonfindel <glonfindel1995@gmail.com>
2018-11-19 13:35:27 +0100

    ui energy works

--- Assets/Scripts/CharacterUI.cs
+++ Assets/Scripts/CharacterUI.cs
@@ -7,14 +7,22 @@ public class CharacterUI : MonoBehaviour {
 
     public Image health;
     public Image spiritEnergy;
+    public GameObject highlight;
     private CharacterController player;
 
 	void Start () {
         player = FindObjectOfType<CharacterController>();
         player.Health.OnDamageTaken += UpdateHP;
-	}
+	    player.Energy.OnValueChange += UpdateMP;
+    }
 	
 	void UpdateHP () {
         health.fillAmount = player.Health.HealthAsPercentage;
 	}
+
+    void UpdateMP()
+    {
+        spiritEnergy.fillAmount = player.Energy.EnergyAsPercentage;
+        highlight.SetActive(spiritEnergy.fillAmount == 0.82f);
+    }
 }
