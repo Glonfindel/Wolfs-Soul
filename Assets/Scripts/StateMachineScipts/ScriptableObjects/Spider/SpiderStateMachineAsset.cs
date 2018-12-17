e4dbdce44835d2e74ffe9d2a52abd3e166669cf1
Glonfindel <glonfindel1995@gmail.com>
2018-12-17 12:46:35 +0100

    all basic enemies

---
+++ Assets/Scripts/StateMachineScipts/ScriptableObjects/Spider/SpiderStateMachineAsset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Spider State Machine", menuName = "State Machines/Spider")]
+public partial class SpiderStateMachineAsset : StateMachineAsset
+{
+    public override StateMachine Create(GameObject user)
+    {
+        StateMachine stateMachine = new StateMachine(user);
+
+        CreateIdle(stateMachine);
+        CreateWalkForward(stateMachine);
+        CreateRotateRight(stateMachine);
+        CreateRotateLeft(stateMachine);
+        CreateAttack1(stateMachine);
+        CreateGetHit(stateMachine);
+        CreateDeath(stateMachine);
+
+        return stateMachine;
+    }
+}
