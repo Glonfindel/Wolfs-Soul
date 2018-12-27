3f962f8af66425b65219626b58d1be0dcb9ea14a
Marta Skrajda <m.skrajda.1996@gmail.com>
2018-12-27 17:19:23 +0100

    Oh, it's a trap!

---
+++ Assets/Scripts/CollisionTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrap : Trap {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health)
+        {
+            Activate();
+            health.TakeDamage(damage);
+        }
+        
+    }
+
+    public override void Activate() {
+        anim.SetTrigger("Trigger");
+    }
+}
---
+++ Assets/Scripts/RepeatingTrap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingTrap : Trap
+{
+
+    public float startTime;
+    public float interval;
+    private Animator anim;
+    private Collider col;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        col = GetComponent<Collider>();
+        InvokeRepeating("Activate", startTime, interval);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health)
+        {
+            health.TakeDamage(damage);
+        }
+    }
+    private void Update()
+    {
+        col.enabled = anim.GetFloat("On") == 1;
+    }
+
+    public override void Activate()
+    {
+        anim.SetTrigger("Trigger");
+    }
+}
