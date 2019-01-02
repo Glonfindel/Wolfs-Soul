9e5424d205ca920d351930693574d57d19151f2a
Glonfindel <glonfindel1995@gmail.com>
2019-01-02 18:00:12 +0100

    boss done

---
+++ Assets/Scripts/ColliderEnabler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderEnabler : MonoBehaviour
+{
+
+    public float Time = 1;
+    public float RepeatTime = 1;
+    private Collider coll;
+
+    void Start ()
+    {
+        coll = GetComponent<Collider>();
+        InvokeRepeating("EnableColl", Time, RepeatTime);
+	}
+
+    void EnableColl()
+    {
+        coll.enabled = !coll.enabled;
+    }
+
+}
---
+++ Assets/Scripts/StateMachineScipts/ScriptableObjects/Demon/Attack4State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class DemonStateMachineAsset
+{
+    private void CreateAttack4(StateMachine stateMachine)
+    {
+        State state;
+        Transition transition;
+
+        state = new State("Attack4");
+        stateMachine.AddState(state);
+        state.AddBehaviour(new ExecuteAttackOnAnimCurveBehaviour(new AttackBehaviour("Attack4")));
+        state.AddBehaviour(new PlayAnimationBehaviour("Attack4", 0.1f));
+        state.AddBehaviour(new RandomAttackBehaviour());
+
+        transition = new Transition("Idle");
+        state.AddTransition(transition);
+        transition.AddCondition(new AnimationFinishedCondition());
+    }
+}
