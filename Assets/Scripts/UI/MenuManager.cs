361a43794995e5207980248079a5ef0708cd83d4
Glonfindel <glonfindel1995@gmail.com>
2019-01-03 19:25:10 +0100

    load game prepare

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour {
+public class MenuManager : MonoBehaviour
+{
 
-	public void LoadScene(string sceneName)
+    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    public void LoadGame(int index)
+    {
+        string level = PlayerPrefs.GetString("Save" + index);
+        if (!string.IsNullOrEmpty(level))
+            SceneManager.LoadScene(level);
     }
     public void Disappear(GameObject go)
     {
