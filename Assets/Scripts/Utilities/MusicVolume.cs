46e7805b4766eb45b08fd5f7dc6791d4ff648d1c
Glonfindel <glonfindel1995@gmail.com>
2019-01-07 11:33:34 +0100

    audio volume

---
+++ Assets/Scripts/Utilities/MusicVolume.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicVolume : MonoBehaviour {
+
+    public void Awake()
+    {
+        GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Music", 0.5f);
+    }
+}
---
+++ Assets/Scripts/Utilities/SoundVolume.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundVolume : MonoBehaviour {
+
+    public void Awake()
+    {
+        GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("Sound", 0.5f);
+    }
+}
