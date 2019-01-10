36b95d1c02ed73ed3099ca0ff0b88d5601e9e21c
Glonfindel <glonfindel1995@gmail.com>
2019-01-10 16:46:34 +0100

    pad input done

--- Assets/Scripts/WinGameScript.cs
+++ Assets/Scripts/WinGameScript.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class WinGameScript : MonoBehaviour
 {
-
-    private PauseManager uiManager;
-
-	void Start ()
-	{
-	    uiManager = FindObjectOfType<PauseManager>();
-	}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CharacterController>())
         {
-            uiManager.WinPanel.SetActive(true);
+            UIManager.Instance.WinPanel.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(UIManager.Instance.WinPanel.GetComponentInChildren<Selectable>().gameObject);
             Camera.main.transform.parent = null;
             other.gameObject.SetActive(false);
         }
